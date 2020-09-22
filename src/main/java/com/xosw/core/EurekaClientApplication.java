646b54d3bb14b9ba5d2cee7e50af85377023311c
zuoyao <zuoyao@xosw.com>
2020-09-23 03:49:14 +0800

    client config

--- src/main/java/com/xosw/core/EurekaClientApplication.java
+++ src/main/java/com/xosw/core/EurekaClientApplication.java
@@ -1,13 +1,20 @@
 package com.xosw.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaClientApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(EurekaClientApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
+        logger.info("启动微服务EurekaClient");
     }
 
 }
